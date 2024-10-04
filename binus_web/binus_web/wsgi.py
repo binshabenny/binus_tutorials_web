5407c967001496eef1654b1d263a7d1371c3d8e4
Binsha Benny <binshabenny3@gmail.com>
2024-10-04 18:21:36 +0530

    wsgi file changed

--- binus_web/binus_web/wsgi.py
+++ binus_web/binus_web/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'binus_web.settings')
 
 application = get_wsgi_application()
+
+
